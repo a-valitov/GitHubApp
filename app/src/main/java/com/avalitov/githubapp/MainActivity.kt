54433f4a1d1f7d7f6c81c28dfbb1631923824b79
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-12-10 16:10:35 +0500

    Search via EditTextView realized

--- app/src/main/java/com/avalitov/githubapp/MainActivity.kt
+++ app/src/main/java/com/avalitov/githubapp/MainActivity.kt
@@ -3,7 +3,10 @@ package com.avalitov.githubapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.avalitov.githubapp.model.RecyclerAdapter
@@ -20,21 +23,25 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
 
     lateinit var tvText : TextView
+    lateinit var btnSearch : ImageView
+    lateinit var etSearch : EditText
+
     var reposArrayList = arrayListOf<Repository>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         tvText = findViewById(R.id.tv_text)
+        btnSearch = findViewById(R.id.btn_search)
+        etSearch = findViewById(R.id.et_search)
 
-        getRepositoriesFromSearch()
-
-//        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-//        recyclerView.layoutManager = LinearLayoutManager(this)
-//        recyclerView.layoutManager = LinearLayoutManager(this)
-//        recyclerView.adapter = RecyclerAdapter(reposArrayList)
-
-        //getUsers()
+        btnSearch.setOnClickListener {
+            val query = etSearch.text.toString()
+            if (query.isNotEmpty()) {
+                getRepositoriesFromSearch(query)
+            }
+        }
 
     }
 
@@ -67,34 +74,26 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun getRepositoriesFromSearch(){
+    private fun getRepositoriesFromSearch(query : String){
+
         val retrofitBuilder = Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(BASE_URL)
                 .build()
                 .create(ApiInterface::class.java)
 
-        val retrofitData = retrofitBuilder.getRepositories()
+        val retrofitData = retrofitBuilder.getRepositories(query)
         retrofitData.enqueue(object : Callback<SearchRepoResponse?> {
             override fun onResponse(call: Call<SearchRepoResponse?>, response: Response<SearchRepoResponse?>) {
                 val responseBody = response.body()?.items
 
                 if (responseBody != null) {
-//                    val userStringBuilder = StringBuilder()
-//                    for (dataUnit in responseBody) {
-//                        userStringBuilder.append(dataUnit.name)
-//                        userStringBuilder.append(" ")
-//                        userStringBuilder.append(dataUnit.owner.login)
-//                        userStringBuilder.append("\n")
-//                    }
-//
-//                    tvText.text = userStringBuilder
                     reposArrayList = responseBody
-
                     val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
                     recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
                     recyclerView.adapter = RecyclerAdapter(reposArrayList)
-
+                } else {
+                    Toast.makeText(this@MainActivity, "Repositories not found.", Toast.LENGTH_SHORT)
                 }
             }
 
